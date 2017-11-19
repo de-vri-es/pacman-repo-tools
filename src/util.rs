2567a2ed895c243a13c5ae87d5dfe30bb27bacf0
Maarten de Vries <maarten@de-vri.es>
2017-11-19 23:29:23 +0100

    Implement and test version comparison.

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,12 @@
-use std::str::pattern::{Pattern,ReverseSearcher,SearchStep,Searcher};
+use std::str::pattern::{Pattern,ReverseSearcher,Searcher};
 
 
 pub trait ConsumableStr<'a> : Copy {
-	fn consume_front_n(self, n: usize) -> (Option<&'a str>, &'a str);
-	fn consume_back_n(self, n: usize) -> (Option<&'a str>, &'a str);
+	fn consume_front_n(&mut self, n: usize) -> Option<&'a str>;
+	fn consume_back_n(&mut self, n: usize) -> Option<&'a str>;
 
-	fn consume_front<P: Pattern<'a>>(self, pattern: P) -> (Option<&'a str>, &'a str);
-	fn consume_back<P: Pattern<'a>>(self, pattern: P) -> (Option<&'a str>, &'a str)
+	fn consume_front_while<P: Pattern<'a>>(&mut self, pattern: P) -> &'a str;
+	fn consume_back_while<P: Pattern<'a>>(&mut self, pattern: P) -> &'a str
 		where P::Searcher: ReverseSearcher<'a>;
 
 	fn  partition<P: Pattern<'a>>(self, pattern: P) -> Option<(&'a str, &'a str, &'a str)>;
@@ -15,36 +15,35 @@ pub trait ConsumableStr<'a> : Copy {
 }
 
 impl<'a> ConsumableStr<'a> for &'a str {
-	fn consume_front_n(self, n: usize) -> (Option<&'a str>, &'a str) {
-		if self.len() < n { (None, self) }
-		else { (Some(&self[0..n]), &self[n..]) }
+	fn consume_front_n(&mut self, n: usize) -> Option<&'a str> {
+		if self.len() < n { return None }
+		let result = Some(&self[..n]);
+		*self = &self[n..];
+		result
 	}
 
-	fn consume_back_n(self, n: usize) -> (Option<&'a str>, &'a str) {
-		if self.len() < n {
-			(None, self)
-		} else {
-			let index = self.len() - 1 - n;
-			(Some(&self[index..]), &self[..index])
-		}
+	fn consume_back_n(&mut self, n: usize) -> Option<&'a str> {
+		if self.len() < n { return None }
+		let index = self.len() - n;
+		let result = Some(&self[index..]);
+		*self = &self[..index];
+		result
 	}
 
-	fn consume_front<P: Pattern<'a>> (self, pattern: P) -> (Option<&'a str>, &'a str) {
-		if let SearchStep::Match(_, i) = pattern.into_searcher(self).next() {
-			(Some(&self[..i]), &self[i..])
-		} else {
-			(None, self)
-		}
+	fn consume_front_while<P: Pattern<'a>> (&mut self, pattern: P) -> &'a str {
+		let i = pattern.into_searcher(self).next_reject().map(|(i, _)| i).unwrap_or(self.len());
+		let (left, right) = self.split_at(i);
+		*self = right;
+		left
 	}
 
-	fn consume_back<P: Pattern<'a>> (self, pattern: P) -> (Option<&'a str>, &'a str)
+	fn consume_back_while<P: Pattern<'a>> (&mut self, pattern: P) -> &'a str
 		where P::Searcher: ReverseSearcher<'a>
 	{
-		if let SearchStep::Match(i, _) = pattern.into_searcher(self).next_back() {
-			(Some(&self[i..]), &self[i..])
-		} else {
-			(None, self)
-		}
+		let i = pattern.into_searcher(self).next_reject_back().map(|(_, i)| i).unwrap_or(0);
+		let (left, right) = self.split_at(i);
+		*self = left;
+		right
 	}
 
 	fn partition<P: Pattern<'a>>(self, pattern: P) -> Option<(&'a str, &'a str, &'a str)> {
@@ -65,6 +64,106 @@ impl<'a> ConsumableStr<'a> for &'a str {
 #[cfg(test)]
 mod tests {
 	use super::ConsumableStr;
+
+	#[test]
+	fn consume_front_n() {
+		let data = String::from("abcdef");
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_n(0), Some(""));
+			assert_eq!(a, "abcdef");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_n(3), Some("abc"));
+			assert_eq!(a, "def");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_n(6), Some("abcdef"));
+			assert_eq!(a, "");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_n(7), None);
+			assert_eq!(a, "abcdef");
+		}
+	}
+
+	#[test]
+	fn consume_back_n() {
+		let data = String::from("abcdef");
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_n(0), Some(""));
+			assert_eq!(a, "abcdef");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_n(3), Some("def"));
+			assert_eq!(a, "abc");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_n(6), Some("abcdef"));
+			assert_eq!(a, "");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_n(7), None);
+			assert_eq!(a, "abcdef");
+		}
+	}
+
+	#[test]
+	fn consume_front_while() {
+		let data = String::from("aaabbb");
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_while('a'), "aaa");
+			assert_eq!(a, "bbb");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_while(|c| c == 'a'), "aaa");
+			assert_eq!(a, "bbb");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_while('b'), "");
+			assert_eq!(a, "aaabbb");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_front_while(|c| c == 'a' || c == 'b'), "aaabbb");
+			assert_eq!(a, "");
+		}
+	}
+
+	#[test]
+	fn consume_back_while() {
+		let data = String::from("aaabbb");
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_while('b'), "bbb");
+			assert_eq!(a, "aaa");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_while(|c| c == 'b'), "bbb");
+			assert_eq!(a, "aaa");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_while('a'), "");
+			assert_eq!(a, "aaabbb");
+		}
+		{
+			let mut a: &str = data.as_ref();
+			assert_eq!(a.consume_back_while(|c| c == 'a' || c == 'b'), "aaabbb");
+			assert_eq!(a, "");
+		}
+	}
 
 	#[test]
 	fn partition() {
--- src/version.rs
+++ src/version.rs
@@ -7,42 +7,235 @@ struct VersionString<'a>(&'a str);
 impl<'a> From<&'a str> for VersionString<'a> { fn from(s: &'a str) -> VersionString<'a> {VersionString(s)} }
 impl<'a> Into<&'a str> for VersionString<'a> { fn into(self) -> &'a str {self.0} }
 
+impl<'a> Ord        for VersionString<'a> { fn         cmp(&self, other: &VersionString<'a>) -> Ordering { compare_version_string(self.0, other.0) } }
 impl<'a> PartialOrd for VersionString<'a> { fn partial_cmp(&self, other: &VersionString<'a>) -> Option<Ordering> { Some(self.cmp(other)) } }
-impl<'a> Ord        for VersionString<'a> { fn cmp(&self, other: &VersionString<'a>) -> Ordering { compare_version_string(self.0, other.0) } }
-impl<'a> PartialEq  for VersionString<'a> { fn eq(&self, other: &VersionString<'a>) -> bool { self.cmp(other) == Ordering::Equal } }
+impl<'a> PartialEq  for VersionString<'a> { fn          eq(&self, other: &VersionString<'a>) -> bool { self.cmp(other) == Ordering::Equal } }
 impl<'a> Eq         for VersionString<'a> {}
 
 #[derive(PartialEq,PartialOrd,Eq,Ord)]
 struct VersionParts<'a> {
-	epoch: Option<&'a str>,
+	epoch: i32,
 	pkgver: VersionString<'a>,
 	pkgrel: Option<VersionString<'a>>,
 }
 
-fn split_epoch(v: &str) -> (Option<&str>, &str) {
-	v.partition(':').map_or_else(|| (None, v), |(epoch, _, rest)| (Some(epoch), rest))
+fn consume_epoch(v: &mut &str) -> Option<i32> {
+	let mut a: &str = v;
+	let epoch = a.consume_front_while(|x: char| x.is_digit(10));
+	if a.consume_front_n(1) == Some(":") {
+		*v = a;
+		Some(if epoch.is_empty() {0} else {epoch.parse().unwrap()})
+	} else {
+		None
+	}
 }
 
-fn split_pkgrel(v: &str) -> (&str, Option<&str>) {
-	v.rpartition('-').map_or_else(|| (v, None), |(rest, _, pkgrel)| (rest, Some(pkgrel)))
+fn consume_pkgrel<'a>(v: &mut &'a str) -> Option<&'a str> {
+	v.rpartition('-').map(|(rest, _, pkgrel)| {
+		*v = rest;
+		pkgrel
+	})
 }
 
 fn split_parts(version: &str) -> VersionParts {
-	let (epoch,  version) = split_epoch(version);
-	let (pkgver, pkgrel)  = split_pkgrel(version);
-	let pkgver = pkgver.into();
-	let pkgrel = pkgrel.map(|x| x.into());
+	let mut version = version;
+	let epoch  = consume_epoch(&mut version).unwrap_or(0);
+	let pkgrel = consume_pkgrel(&mut version).map(|x| x.into());
+	let pkgver = version.into();
 	VersionParts{epoch, pkgver, pkgrel}
 }
 
-pub fn compare(a: &str, b: &str) -> Ordering {
+pub fn compare_package_version(a: &str, b: &str) -> Ordering {
 	split_parts(a).cmp(&split_parts(b))
 }
 
 pub fn compare_version_string(a: &str, b: &str) -> Ordering {
-	while !a.is_empty() && !b.is_empty() {
+	let mut a = a;
+	let mut b = b;
 
+	// Loop over the alphanumeric parts.
+	while !a.is_empty() || !b.is_empty() {
+		// Get the first alphanumeric component.
+		let mut a_alnum = a.consume_front_while(|x: char| x.is_alphanumeric());
+		let mut b_alnum = b.consume_front_while(|x: char| x.is_alphanumeric());
+
+		// Loop over the numeric and alphabetical parts.
+		while !a_alnum.is_empty() || !b_alnum.is_empty() {
+			let a_num   = a_alnum.consume_front_while(|x: char| x.is_digit(10));
+			let b_num   = b_alnum.consume_front_while(|x: char| x.is_digit(10));
+			let a_alpha = a_alnum.consume_front_while(|x: char| x.is_alphabetic());
+			let b_alpha = b_alnum.consume_front_while(|x: char| x.is_alphabetic());
+
+			// Parse the numeric part.
+			let a_num = if a_num.is_empty() { -1 } else { a_num.parse::<i32>().unwrap() };
+			let b_num = if b_num.is_empty() { -1 } else { b_num.parse::<i32>().unwrap() };
+
+			// If the numeric part is different, we're done.
+			let num_ordering = a_num.cmp(&b_num);
+			if num_ordering != Ordering::Equal { return num_ordering };
+
+			// If the alphabetical part is different, we're done.
+			// Note that an empty alphabetical part is "newer" than a non-empty alphabetical part.
+			let alpha_ordering = (a_alpha.is_empty(), a_alpha).cmp(&(b_alpha.is_empty(), b_alpha));
+			if alpha_ordering != Ordering::Equal { return alpha_ordering };
+		}
+
+		// Drop the non-alphanumeric separator.
+		// If only one has a separator, it's a newer version.
+		let a_sep = a.consume_front_while(|x: char| !x.is_alphanumeric());
+		let b_sep = b.consume_front_while(|x: char| !x.is_alphanumeric());
+		let ordering = (!a_sep.is_empty()).cmp(&!b_sep.is_empty());
+		if ordering != Ordering::Equal { return ordering }
 	}
 
-	return a.cmp(b);
+	// If we get here the versions are equal.
+	Ordering::Equal
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_consume_epoch() {
+		{
+			let mut a = "3:a";
+			assert_eq!(consume_epoch(&mut a), Some(3));
+			assert_eq!(a, "a");
+		}
+		{
+			let mut a = "3:a:b";
+			assert_eq!(consume_epoch(&mut a), Some(3));
+			assert_eq!(a, "a:b");
+		}
+		{
+			let mut a = "31:a";
+			assert_eq!(consume_epoch(&mut a), Some(31));
+			assert_eq!(a, "a");
+		}
+		{
+			let mut a = "a";
+			assert_eq!(consume_epoch(&mut a), None);
+			assert_eq!(a, "a");
+		}
+		{
+			let mut a = "abc";
+			assert_eq!(consume_epoch(&mut a), None);
+			assert_eq!(a, "abc");
+		}
+		{
+			let mut a = "3a1:a";
+			assert_eq!(consume_epoch(&mut a), None);
+			assert_eq!(a, "3a1:a");
+		}
+	}
+
+	#[test]
+	fn test_consume_pkgrel() {
+		{
+			let mut a = "1-2";
+			assert_eq!(consume_pkgrel(&mut a), Some("2"));
+			assert_eq!(a, "1");
+		}
+		{
+			let mut a = "1-2-3";
+			assert_eq!(consume_pkgrel(&mut a), Some("3"));
+			assert_eq!(a, "1-2");
+		}
+		{
+			let mut a = "1.2abc-3.4def";
+			assert_eq!(consume_pkgrel(&mut a), Some("3.4def"));
+			assert_eq!(a, "1.2abc");
+		}
+		{
+			let mut a = "123";
+			assert_eq!(consume_pkgrel(&mut a), None);
+			assert_eq!(a, "123");
+		}
+	}
+
+	fn assert_compare_version_string(a: &str, b: &str, ordering: Ordering) {
+		assert_eq!(compare_version_string(a, b), ordering, "comparing {:?} to {:?}", a, b);
+		assert_eq!(compare_version_string(b, a), ordering.reverse(), "comparing {:?} to {:?}", a, b);
+		assert_eq!(VersionString(a).cmp(&VersionString(b)), ordering, "comparing {:?} to {:?}", b, a);
+		assert_eq!(VersionString(b).cmp(&VersionString(a)), ordering.reverse(), "comparing {:?} to {:?}", b, a);
+	}
+
+	#[test]
+	fn test_compare_version_string() {
+		// Simple cases.
+		assert_compare_version_string("",  "",  Ordering::Equal);
+		assert_compare_version_string("1", "0", Ordering::Greater);
+		assert_compare_version_string("0", "1", Ordering::Less);
+
+		// Examples from the man page for alphanumeric comparisons.
+		assert_compare_version_string("1.0a"   , "1.0b"   , Ordering::Less);
+		assert_compare_version_string("1.0b"   , "1.0beta", Ordering::Less);
+		assert_compare_version_string("1.0beta", "1.0p"   , Ordering::Less);
+		assert_compare_version_string("1.0p"   , "1.0pre" , Ordering::Less);
+		assert_compare_version_string("1.0pre" , "1.0rc"  , Ordering::Less);
+		assert_compare_version_string("1.0rc"  , "1.0"    , Ordering::Less);
+		assert_compare_version_string("1.0"    , "1.0.a"  , Ordering::Less);
+		assert_compare_version_string("1.0.a"  , "1.0.1"  , Ordering::Less);
+
+		// Examples from the man page for numeric comparisons.
+		assert_compare_version_string("1"    , "1.0"  , Ordering::Less);
+		assert_compare_version_string("1.0"  , "1.1"  , Ordering::Less);
+		assert_compare_version_string("1.1"  , "1.1.1", Ordering::Less);
+		assert_compare_version_string("1.1.1", "1.2"  , Ordering::Less);
+		assert_compare_version_string("1.2"  , "2.0"  , Ordering::Less);
+		assert_compare_version_string("2.0"  , "3.0.0", Ordering::Less);
+
+		// Extra numeric component makes the version greater.
+		assert_compare_version_string("1.0rc", "1.0rc1", Ordering::Less);
+
+		// Extra alphabetical component makes the version less.
+		assert_compare_version_string("1a2b", "1a2", Ordering::Less);
+
+		// Extra seperator makes the version greater.
+		assert_compare_version_string("1", "1.", Ordering::Less);
+
+		// Consecutive separators are folded into one separator.
+		assert_compare_version_string("1." , "1..",  Ordering::Equal);
+		assert_compare_version_string("1.2", "1..2", Ordering::Equal);
+
+		// Empty components are newer than alphabetical components.
+		assert_compare_version_string("1..a" , "1."    , Ordering::Less);
+		assert_compare_version_string("1..a" , "1.."   , Ordering::Less);
+		// Empty components are less than numeric components.
+		assert_compare_version_string("1.."  , "1..1"  , Ordering::Less);
+	}
+
+	fn assert_compare_package_version(a: &str, b: &str, ordering: Ordering) {
+		assert_eq!(compare_package_version(a, b), ordering, "comparing {:?} to {:?}", a, b);
+		assert_eq!(compare_package_version(b, a), ordering.reverse(), "comparing {:?} to {:?}", b, a);
+	}
+
+	#[test]
+	fn test_compare_package_version() {
+		// Test simple cases.
+		assert_compare_package_version("",  "" , Ordering::Equal);
+		assert_compare_package_version("1", "0", Ordering::Greater);
+		assert_compare_package_version("0", "1", Ordering::Less);
+
+		// Test that pkgrel decides if pkgver is equal.
+		assert_compare_package_version("0-1", "0",   Ordering::Greater);
+		assert_compare_package_version("0-1", "0-0", Ordering::Greater);
+		assert_compare_package_version("1-0", "0-1", Ordering::Greater);
+
+		// Test that epoch is implicitly 0.
+		assert_compare_package_version("0:",  "" , Ordering::Equal);
+		assert_compare_package_version("0:1", "0", Ordering::Greater);
+		assert_compare_package_version("0:0", "1", Ordering::Less);
+
+		// Test that epoch trumps pkgver.
+		assert_compare_package_version("1:1", "0", Ordering::Greater);
+		assert_compare_package_version("1:0", "0", Ordering::Greater);
+		assert_compare_package_version("1:0", "1", Ordering::Greater);
+
+		// Test that epoch trumps pkgrel.
+		assert_compare_package_version("0-1", "1:0",   Ordering::Less);
+		assert_compare_package_version("0-1", "1:0-0", Ordering::Less);
+		assert_compare_package_version("1-0", "1:0-1", Ordering::Less);
+	}
 }
